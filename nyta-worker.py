3b275bd6c0e6b5776626eea54c55432bdd584ef2
Brian Farley <bmfarley@subobcens.us>
2017-12-22 20:23:08 -0600

    Added code to push to RDS

--- nyta-worker.py
+++ nyta-worker.py
@@ -1,5 +1,7 @@
 import boto3
 import requests
+import psycopg2 as pg2
+from psycopg2.extras import execute_values
 import json
 import itertools
 
@@ -8,6 +10,12 @@ API_URL = 'https://api.nytimes.com/svc/archive/v1/{year:}/{month:}.json'
 PAYLOAD = {'api-key':API_KEY}
 S3_BUCKET = 'bf-nyt-archive'
 SNS_NOTIFIER = 'redacted'
+
+nyt_creds = {'dbname':'nyt_articles',
+             'port':5432,
+             'user':'dbadmin',
+             'password':'redacted',
+             'host':'nyt-articles.ckwrv3en1yz3.us-east-2.rds.amazonaws.com'}
 
 def initialize():
     ohio_session = boto3.Session(region_name='us-east-2')
@@ -43,9 +51,39 @@ def nyta_response_to_s3(year,month,sns_client,s3_resource):
     sns_client.publish(PhoneNumber=SNS_NOTIFIER,
                    Message='[nyt-archive-worker]: {}/{:02d} successfully published {} docs'.format(year,month,doc_count))
 
+def s3_to_rds(year,month,sns_client,s3_resource):
+    prefix = f'{year}/{str(month).zfill(2)}/'
+    records = []
+    for obj in s3_resource.objects.filter(Prefix=prefix):
+        content_object = s3_resource.Object('bf-nyt-archive', obj.key)
+        file_content = content_object.get()['Body'].read().decode('UTF-8')
+        json_content = json.loads(file_content)
+        pp = json_content.get('print_page', None)
+        lp = json_content.get('lead_paragraph', None)
+        sn = json_content.get('snippet', None)
+        hd = json_content['headline'].get('main', None)
+        pd = json_content.get('pub_date', None)
+        _id = json_content.get('_id', None)
+        records.append((_id, pp, sn, lp, hd, pd))
+
+    insert_meta = '''INSERT INTO article_meta (article_id,print_page,snippet,lead_paragraph,headline,publication_time)
+VALUES %s'''
+    with pg2.connect(**nyt_creds) as nyt_conn:
+        cursor = nyt_conn.cursor()
+        execute_values(cursor,insert_meta,records)
+
+    sns_client.publish(PhoneNumber=SNS_NOTIFIER,
+                       Message='[nyt-archive-worker]: {}/{} uploaded to postgres'.format(year,str(month).zfill(2)))
+
+
 
 if __name__ == '__main__':
     s3, sns_client = initialize()
+    print('1) Get articles from NYT\n2) Move articles to RDS')
+    choice_menu = {'1':nyta_response_to_s3,
+                   '2':s3_to_rds}
+    choice = input('Do which?  ').rstrip()
+    action = choice_menu[choice]
     year_start = int(input('Start from which year?  ').rstrip())
     month_start = int(input('Start from which month?  ').rstrip())
     reach_back = int(input('Reach back how many months?  ').rstrip())
@@ -61,7 +99,7 @@ if __name__ == '__main__':
 
     for y,m in to_process:
         try:
-            nyta_response_to_s3(y,m,sns_client,s3)
+            action(y,m,sns_client,s3)
         except Exception as e:
             sns_client.publish(PhoneNumber=SNS_NOTIFIER,
                                Message='[nyt-archive-worker]: FATAL {}'.format(e))
