85d98966a214e0db979b52f86696f2c0d0efda0e
Brian Farley <bmfarley@subobcens.us>
2017-12-22 20:42:45 -0600

    Fixed some bugs.

--- nyta-worker.py
+++ nyta-worker.py
@@ -55,7 +55,8 @@ def s3_to_rds(year,month,sns_client,s3_resource):
     prefix = f'{year}/{str(month).zfill(2)}/'
     records = []
     for obj in s3_resource.objects.filter(Prefix=prefix):
-        content_object = s3_resource.Object('bf-nyt-archive', obj.key)
+        bucket = s3_resource.Bucket(name=S3_BUCKET)
+        content_object = s3_resource.Object(S3_BUCKET, obj.key)
         file_content = content_object.get()['Body'].read().decode('UTF-8')
         json_content = json.loads(file_content)
         pp = json_content.get('print_page', None)
@@ -103,3 +104,4 @@ if __name__ == '__main__':
         except Exception as e:
             sns_client.publish(PhoneNumber=SNS_NOTIFIER,
                                Message='[nyt-archive-worker]: FATAL {}'.format(e))
+            raise
